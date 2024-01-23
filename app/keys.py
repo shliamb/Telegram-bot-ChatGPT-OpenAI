c1308d3f1bbb4ad60d6984f959c7116551018fec
Shaposhnikov Alexander <firstremont@gmail.com>
2024-01-23 15:08:32 +0800

    del

--- app/keys.py
+++
@@ -1,9 +0,0 @@
-import os
-import telebot
-
-# Telegram key
-token = os.environ.get('TELEGRAM_BOT_CHATGPT_API_KEY')
-bot = telebot.TeleBot(token)
-
-# OpenAI key-token
-api_key = os.environ.get('CHATGPT_API_KEY')
