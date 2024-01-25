80a8f66a047cc04153c11fe5f0dd551ca9f3fc3a
Shaposhnikov Alexander <firstremont@gmail.com>
2024-01-25 19:23:04 +0800

    25jan2

--- app/tele_bot.py
+++ app/tele_bot.py
@@ -1,6 +1,6 @@
 import telebot
 from keys import token
-from worker_db import read_tele_user, add_tele_user
+from worker_db import read_tele_user, add_tele_user, add_chatgpt_setings
 
 
 bot = telebot.TeleBot(token) # Conection for API Telegram
@@ -8,31 +8,31 @@ bot = telebot.TeleBot(token) # Conection for API Telegram
 
 
 
-# Вызов меню /start
+# /start
 @bot.message_handler(commands=['start'])
 def start(context):
     # Проверяем где есть имя у Телегарм польз-ля
     if context.from_user.username:
         about = context.from_user.username
-    elif context.message.from_user.first_name:
-        about = context.message.from_user.first_name
+    elif context.from_user.first_name:
+        about = context.from_user.first_name
     elif context.from_user.last_name:
         about = context.from_user.last_name
     else:
         about = "друг"
     # Выводим привествие пользователю
-    bot.send_message(context.chat.id, text=f"Привет, {about}! Добро пожаловать в ChatGPT бота.")
-    # Если в базе нет такого id то добавляем
-    user_id = context.from_user.id # телеграм id нажавшего старт
-    read_id = read_tele_user(user_id) # считываем в базе с таким id все его данные
+    bot.send_message(context.chat.id, text=f"Привет, {about}! Я ChatGPT бот.\nМне можно сразу задать вопрос\nили настроить - /setup.")
+    user_id = context.from_user.id # Телеграм id нажавшего старт
+    read_id = read_tele_user(user_id) # Считываем в базе с таким id все его данные
     # user_id_1, user_username_1, user_db_id_1 = read_id # Теперь вы можете извлечь значения из кортежа
     # print(user_id_1)
+    # Если в базе нет такого id то добавляем
     if read_id == None:
-        add_tele_user(context.from_user.id, context.from_user.username )
-        print("Записал")
-    else:
-        print("Уже есть такой тип")
-    
+        add_tele_user(context.from_user.id, context.from_user.username, context.from_user.first_name, context.from_user.last_name)
+        # Вычитываю из таблицы UserTelegram только что добавленного пользователя, а конкретно id следуюет 4ым в кортеже
+        added_user_id = read_tele_user(user_id)[4] # Получили id только что внесенного пользователя
+        # Вносим id в таблицу настроек gpt, нужное проставиться по default
+        add_chatgpt_setings(added_user_id)
 
 
 
@@ -42,8 +42,6 @@ def start(context):
     # user_last_name = context.from_user.last_name  # Фамилия пользователя (может быть None)
     # user_username = context.from_user.username  # Юзернейм пользователя (может быть None)
     # chat_id = context.chat.id
-
-
 
 
 
--- app/worker_db.py
+++ app/worker_db.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import relationship, sessionmaker
-from models import UsersTelegram, engine
+from models import UsersTelegram, ChatGpt, engine
 
 
 # Conection DB
@@ -8,14 +8,20 @@ session = Session()
 
 
 # Reading User Telegram in a Base
-def read_tele_user(absd):
-    users = session.query(UsersTelegram).filter(UsersTelegram.user_id == absd).all()
+def read_tele_user(data):
+    users = session.query(UsersTelegram).filter(UsersTelegram.user_id == data).all()
     for user in users:
         #print(f'User: {user.user_id}, Email: {user.user_username}, id: {user.id}')
-        return user.user_id, user.user_username, user.id
+        return user.user_id, user.user_username, user.user_first_name, user.user_last_name, user.id
 
 # Writing User Telegram to the Base
-def add_tele_user(user_id, user_username):
-    new_user = UsersTelegram(user_id=user_id, user_username=user_username)
+def add_tele_user(user_id, user_username, user_first_name, user_last_name):
+    new_user = UsersTelegram(user_id=user_id, user_username=user_username, user_first_name=user_first_name, user_last_name=user_last_name)
+    session.add(new_user)
+    session.commit()
+
+# Writing Setings ChatGpt to the Base
+def add_chatgpt_setings(id):
+    new_user = ChatGpt(id=id)
     session.add(new_user)
     session.commit()
