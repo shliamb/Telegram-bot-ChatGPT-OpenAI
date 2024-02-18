555e9c1d39d19a2868980d75da700c32a4ff5f0b
Shaposhnikov Alexander <firstremont@gmail.com>
2024-02-18 10:34:28 +0800

    18Febr-continue

--- myvenv/lib/python3.11/site-packages/aiogram/__meta__.py
+++
@@ -1,2 +0,0 @@
-__version__ = "3.3.0"
-__api_version__ = "7.0"
--- myvenv/lib/python3.11/site-packages/yoomoney/operation_details/digital_bonus.py
+++
@@ -1,9 +0,0 @@
-
-
-class DigitalBonus:
-    def __init__(self,
-                 serial: str,
-                 secret: str
-                 ):
-        self.serial = serial
-        self.secret = secret
