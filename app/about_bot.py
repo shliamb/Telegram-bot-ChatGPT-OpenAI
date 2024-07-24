f4663f59799bc91085b95866948c0ddfa43f032e
Shaposhnikov Alex <firstremont@gmail.com>
2024-07-24 13:28:43 +0300

    24jun2

--- app/about_bot.py
+++ app/about_bot.py
@@ -2,9 +2,10 @@
 
 
 about_text = ( f"\
+\nRU:\
 <b>🤙🏼 Привет!</b>\n\n\
 Бот ChatGPT использует официальное API OpenAI без изменений.\n\n На балансе есть небольшая сумма для проверки ChatGPT, но вы всегда можете\
- пополнить его.\n\n\
+ пополнить его.\n\
 \n\
 <b>Цены на 1к токенов (вход + выход):</b>\n\
 gpt-4o-mini  -  0.0015$ (Рекомендую!)\n\
@@ -18,4 +19,25 @@ gpt-4-0125-preview  -  0.08$\n\
 Если у вас возникли вопросы или проблемы, то напишите мне - @Shliamb.\
 \n\
 \n\
-Git моего инди-проекта - https://github.com/shliamb/Telegram-bot-ChatGPT-OpenAI")
+Git моего инди-проекта - https://github.com/shliamb/Telegram-bot-ChatGPT-OpenAI\
+\n\
+\n\
+\n\
+\nENG:\
+<b>🤙🏼 Hi!</b>\n\n\
+The Chatbot bot uses the official Openapi API without changes.\n\n There is a small amount on the balance to check the ChatGPT, but you can always
+top it up.\n\
+\n\
+<b>Prices for 1k tokens (input + output):</b>\n\
+gpt-4o-mini - 0.0015$ (I recommend it!)\n\
+GPT-4O - 0.04$\n\
+gp-3.5-turbo-0613 - 0.006$\n\
+gpt-3.5-turbo-0125  -  0.004$\n\
+gpt-4-0613  -  0.18$\n\
+gpt-4-1106-preview  -  0.08$\n\
+gpt-4-0125-preview  -  0.08$\n\
+\n\
+If you have any questions or problems, please write to me - @Shliamb.\
+\n\
+\n\
+Git of my indie project - https://github.com/shliamb/Telegram-bot-ChatGPT-OpenAI ")
--- app/terms_of_use.py
+++ app/terms_of_use.py
@@ -1,5 +1,6 @@
 
 terms = ( f"\
+RU:\n\
 <b>Условия использования:</b>\n\n\
 1. Пользование ботом ChatGPT означает ваше согласие с этими условиями использования.\n\n\
 2. ChatGPT предназначен только для развлекательных и информационных целей. Не используйте его для осуществления финансовых транзакций или передачи конфиденциальной информации.\n\n\
@@ -14,6 +15,24 @@ terms = ( f"\
 <b>Контакты поддержки:</b>\n\n\
 Если у вас возникли вопросы, проблемы или предложения, пожалуйста, обратитесь ко мне:\n\n\
 Телеграмм: @Shliamb\n\
+\n\
+\n\   
+\n\
+ENG:\n\
+<b>Terms of use:</b>\n\n\
+1. Using the ChatGPT bot means that you agree to these terms of use.\n\n\
+2. ChatGPT is intended for entertainment and informational purposes only. Do not use it to make financial transactions or transfer confidential information.\n\n\
+3. We are not responsible for any losses that may arise as a result of using the bot.\n\n\
+4. Users are responsible for the content and context of their messages sent through the bot.\n\n\
+5. We reserve the right to make changes to the terms of use without prior notice.\n\n\n\
+<b>Terms of purchase:</b>\n\n\
+1. Payment via ChatGPT 4 bot is intended only for the purchase of virtual goods or services.\n\n\
+2. All information about prices and terms of purchase is available in the relevant sections of the bot.\n\n\
+3. No refund is provided after the purchase, except in the cases described in the refund policy.\n\n\
+4. We guarantee the security of your financial data and ensure the confidentiality of payment information.\n\n\n\
+<b>Support Contacts:</b>\n\n\
+If you have any questions, problems or suggestions, please contact me:\n\n\
+Telegram: @Shliamb\n\
 ")
 
 # Электронная почта: shliambkz@gmail.com\n\
