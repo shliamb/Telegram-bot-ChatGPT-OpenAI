aff6c986bcd70eafbe5c4c2d81a3828f15be1f52
Shaposhnikov Alex <firstremont@gmail.com>
2024-07-24 13:59:27 +0300

    24jun16

--- app/about_bot.py
+++ app/about_bot.py
@@ -24,19 +24,18 @@ Git моего инди-проекта - https://github.com/shliamb/Telegram-bot
 <b>ENG:🤙🏼 Hi!</b>\n\n\
 The Chatbot bot uses the official Openapi API without changes.\n\n There is a small amount on the balance to check the ChatGPT, but you can always\
 top it up.\n\
+\n\
+<b>Prices for 1k tokens (input + output):</b>\n\
+gpt-4o-mini - 0.0015$ (I recommend it!)\n\
+GPT-4O - 0.04$\n\
+gp-3.5-turbo-0613 - 0.006$\n\
+gpt-3.5-turbo-0125  -  0.004$\n\
+gpt-4-0613  -  0.18$\n\
+gpt-4-1106-preview  -  0.08$\n\
+gpt-4-0125-preview  -  0.08$\n\
+\n\
+If you have any questions or problems, please write to me - @Shliamb.\
+\n\
+\n\
+Git of my indie project - https://github.com/shliamb/Telegram-bot-ChatGPT-OpenAI  \
 ")
-# \n\
-# <b>Prices for 1k tokens (input + output):</b>\n\
-# gpt-4o-mini - 0.0015$ (I recommend it!)\n\
-# GPT-4O - 0.04$\n\
-# gp-3.5-turbo-0613 - 0.006$\n\
-# gpt-3.5-turbo-0125  -  0.004$\n\
-# gpt-4-0613  -  0.18$\n\
-# gpt-4-1106-preview  -  0.08$\n\
-# gpt-4-0125-preview  -  0.08$\n\
-# \n\
-# If you have any questions or problems, please write to me - @Shliamb.\
-# \n\
-# \n\
-# Git of my indie project - https://github.com/shliamb/Telegram-bot-ChatGPT-OpenAI  \
-# ")
